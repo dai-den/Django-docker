f0b5c07e8baee51116c00bfe830a1dd45e617f3a
daiki <66196065+dai-den@users.noreply.github.com>
2021-06-13 17:46:34 +0900

    hoge

--- prod/testproject/settings.py
+++ prod/testproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lpu=ez2m6k(umxav96hgfl8mffo*a0u$+3vu7km!akj=*li^i&'
+SECRET_KEY = 'hogehoge'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
